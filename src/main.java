9f056fc71e70bdd67625217c946571a74b539b8d
poojan@poojanshah.com <poojan@poojanshah.com>
2017-08-15 20:43:24 +0100

    checking prime

--- src/main.java
+++ src/main.java
@@ -24,9 +24,19 @@ public class main {
 			}
 		
 		for ( String word : occurrences.keySet() ) {
+			if(isPrime(occurrences.get(word)))
 			System.out.println(word + " : " + occurrences.get(word));
 			
 			}
 	}
+	
+	private static boolean isPrime(int num) {
+        if (num < 2) return false;
+        if (num == 2) return true;
+        if (num % 2 == 0) return false;
+        for (int i = 3; i * i <= num; i += 2)
+            if (num % i == 0) return false;
+        return true;
+}
 
 }
